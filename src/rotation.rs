b15e93b44846b6dc679c378e50baf87be7c436bb
Liang Wang <liang.wang.ice@gmail.com>
2023-03-31 01:36:55 -0400

    all work of chatGPT

--- src/rotation.rs
+++ src/rotation.rs
@@ -6,32 +6,66 @@ use web_sys::ImageData;
 /// Rotate the image by radian, enlarge the image to fit the rotated image, empty spaces are filled
 /// using bilinear interpolation.
 pub fn rotate_rad(image: &CanvasImage, radian: f64) -> CanvasImage {
-    // first reduce the radian to the range of [0, 2 * PI)
     let width = image.width() as f64;
     let height = image.height() as f64;
 
-    let (new_width, new_height) = (
-        (width * radian.cos() + height * radian.sin()).ceil() as u32 + 1,
-        (width * radian.sin() + height * radian.cos()).ceil() as u32 + 1,
+    let (new_width, new_height) = {
+        // calculate the rotated image bounds
+        let corners = [(0.0, 0.0), (0.0, height), (width, 0.0), (width, height)];
+        let rotated_corners = corners.iter().map(|&(x, y)| {
+            let new_x = x * radian.cos() - y * radian.sin();
+            let new_y = x * radian.sin() + y * radian.cos();
+            (new_x, new_y)
+        });
+        let min_x = rotated_corners
+            .clone()
+            .map(|(x, _)| x)
+            .fold(f64::INFINITY, f64::min);
+        let max_x = rotated_corners
+            .clone()
+            .map(|(x, _)| x)
+            .fold(f64::NEG_INFINITY, f64::max);
+        let min_y = rotated_corners
+            .clone()
+            .map(|(_, y)| y)
+            .fold(f64::INFINITY, f64::min);
+        let max_y = rotated_corners
+            .clone()
+            .map(|(_, y)| y)
+            .fold(f64::NEG_INFINITY, f64::max);
+
+        // calculate the size of the new image
+        let new_width = (max_x - min_x).ceil() as u32 + 1;
+        let new_height = (max_y - min_y).ceil() as u32 + 1;
+
+        (new_width, new_height)
+    };
+
+    // image center before rotation
+    let (cx, cy) = (image.width() as f64 / 2f64, image.height() as f64 / 2f64);
+
+    // offset from the center after rotation
+    let (ox, oy) = (
+        (new_width as f64 - width) / 2.0,
+        (new_height as f64 - height) / 2.0,
     );
 
-    let (cx, cy) = (image.width() as f64 / 2f64, image.height() as f64 / 2f64);
-    let (ox, oy) = (image.width() as f64 / 2f64, image.height() as f64 / 2f64);
-
     let rgba = iproduct!(0..new_height, 0..new_width).flat_map(|(y, x)| {
-        let x = x as f64 - cx;
-        let y = y as f64 - cy;
+        let x = x as f64 - cx - ox;
+        let y = cy - y as f64 + oy;
 
-        let x = x * radian.cos() + y * radian.sin();
-        let y = x * radian.sin() + y * radian.cos();
+        // rotate the pixel coordinates
+        let new_x = x * radian.cos() - y * radian.sin();
+        let new_y = x * radian.sin() + y * radian.cos();
 
-        let x = x + cx;
-        let y = y + cy;
+        let x = (new_x + cx).round() as i32;
+        let y = (cy - new_y).round() as i32;
 
-        let x = x + ox;
-        let y = y + oy;
-
-        let pixel = image.rgba(x as u32, y as u32).unwrap_or((128, 64, 32, 0));
+        let pixel = if x >= 0 && x < image.width() as i32 && y >= 0 && y < image.height() as i32 {
+            image.rgba(x as u32, y as u32).unwrap_or((255, 0, 0, 0))
+        } else {
+            (255, 0, 0, 0)
+        };
         [pixel.0, pixel.1, pixel.2, pixel.3]
     });
 
@@ -51,4 +85,35 @@ pub fn rotate(image: ImageData, degree: f64) -> ImageData {
 
     let rotated = rotate_deg(&canvas_image, degree);
     rotated.into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use image::{ImageBuffer, Rgba};
+
+    #[test]
+    fn sanity() {
+        // read the picture from file
+        let image = image::open("meme.png").unwrap();
+
+        // convert to RGBA
+        let image = image.into_rgba8();
+
+        // convert to CanvasImage
+        let width = image.width();
+        let height = image.height();
+        let canvas_image = CanvasImage::from_vec_with_size(image.into_raw(), width, height);
+
+        let rotated = rotate_deg(&canvas_image, 45.0);
+
+        // convert to back to image and save
+        let image: ImageBuffer<Rgba<u8>, &[u8]> = ImageBuffer::from_raw(
+            rotated.width(),
+            rotated.height(),
+            rotated.rgba_slice().clone(),
+        )
+        .unwrap();
+        image.save("meme_rotated.png").unwrap();
+    }
 }
