6b9bde1ede8557ef2303e375218e7f6bf91d8deb
Liang Wang <liang.wang.ice@gmail.com>
2023-04-05 00:36:50 -0400

    even nicer

--- src/rotation.rs
+++ src/rotation.rs
@@ -33,27 +33,7 @@ pub fn rotate_rad(image: &CanvasImage, radian: f64) -> CanvasImage {
     let width = image.width() as f64;
     let height = image.height() as f64;
 
-    let (new_width, new_height) = {
-        // calculate the rotated image bounds
-        let corners = [(0.0, 0.0), (0.0, height), (width, 0.0), (width, height)];
-        let rotated_corners = corners.iter().map(|&(x, y)| rotate_point((x, y), radian));
-
-        let x_minmax = rotated_corners
-            .clone()
-            .map(|(x, _)| x).minmax();
-        let y_minmax = rotated_corners
-            .clone()
-            .map(|(_, y)| y).minmax();
-
-        let (x_min, x_max) = min_max(&x_minmax);
-        let (y_min, y_max) = min_max(&y_minmax);
-
-        // calculate the size of the new image
-        let new_width = (x_max - x_min).ceil() as u32 + 1;
-        let new_height = (y_max - y_min).ceil() as u32 + 1;
-
-        (new_width, new_height)
-    };
+    let (new_width, new_height) = width_height_after_rotation(radian, width, height);
 
     // image center before rotation
     let (cx, cy) = (image.width() as f64 / 2f64, image.height() as f64 / 2f64);
@@ -76,7 +56,6 @@ pub fn rotate_rad(image: &CanvasImage, radian: f64) -> CanvasImage {
         let x = (new_x + cx).round() as i32;
         let y = (new_y + cy).round() as i32;
 
-
         let pixel = if x >= 0 && x < image.width() as i32 && y >= 0 && y < image.height() as i32 {
             image.rgba(x as u32, y as u32).unwrap_or((255, 0, 0, 0))
         } else {
@@ -87,6 +66,31 @@ pub fn rotate_rad(image: &CanvasImage, radian: f64) -> CanvasImage {
 
     let buffer = Vec::from_iter(rgba);
     CanvasImage::from_vec_with_size(buffer, new_width, new_height)
+}
+
+fn width_height_after_rotation(radian: f64, width: f64, height: f64) -> (u32, u32) {
+    let (new_width, new_height) = {
+        // calculate the rotated image bounds
+        let corners = [(0.0, 0.0), (0.0, height), (width, 0.0), (width, height)];
+        let rotated_corners = corners.iter().map(|&(x, y)| rotate_point((x, y), radian));
+
+        let x_minmax = rotated_corners
+            .clone()
+            .map(|(x, _)| x).minmax();
+        let y_minmax = rotated_corners
+            .clone()
+            .map(|(_, y)| y).minmax();
+
+        let (x_min, x_max) = min_max(&x_minmax);
+        let (y_min, y_max) = min_max(&y_minmax);
+
+        // calculate the size of the new image
+        let new_width = (x_max - x_min).ceil() as u32 + 1;
+        let new_height = (y_max - y_min).ceil() as u32 + 1;
+
+        (new_width, new_height)
+    };
+    (new_width, new_height)
 }
 
 /// Rotate the image by degree, enlarge the image to fit the rotated image, empty spaces are filled
