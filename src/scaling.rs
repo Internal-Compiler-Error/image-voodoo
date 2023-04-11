bb141e18df8ecc8040f903d965a88e444e23bc9f
Liang Wang <liang.wang.ice@gmail.com>
2023-04-11 18:33:15 -0400

    can i sleep now?

--- src/scaling.rs
+++ src/scaling.rs
@@ -1,8 +1,8 @@
 #![allow(unused)]
 
 use crate::canvas_image::CanvasImage;
-use crate::interpolation::{bi_nearest_neighbour_interpolation, bilinear_interpolation, lerp, nearest_neighbour_interpolation};
-use itertools::iproduct;
+use crate::interpolation::{lerp};
+use itertools::{iproduct, izip};
 use std::collections::HashSet;
 use std::fmt::format;
 use wasm_bindgen::Clamped;
@@ -14,49 +14,68 @@ pub enum Interpolation {
     Bilinear,
 }
 
+type RGBA = (u8, u8, u8, u8);
+
+fn bi_linear_interpolation(
+    top_left: &RGBA,
+    top_right: &RGBA,
+    bottom_left: &RGBA,
+    bottom_right: &RGBA,
+    x_diff: f64,
+    y_diff: f64,
+) -> [u8; 4] {
+    // can't iterate over tuples so let's turn them into arrays
+    let top_left = [top_left.0, top_left.1, top_left.2, top_left.3];
+    let top_right = [top_right.0, top_right.1, top_right.2, top_right.3];
+    let bottom_left = [bottom_left.0, bottom_left.1, bottom_left.2, bottom_left.3];
+    let bottom_right = [bottom_right.0, bottom_right.1, bottom_right.2, bottom_right.3];
+
+    // another sad fact is we can't guarantee this is a 3 element array
+    let mut rgba: Vec<_> = izip!(top_left, top_right, bottom_left, bottom_right)
+        .take(3)
+        .map(|(top_left, top_right, bottom_left, bottom_right)| {
+            let top = lerp(top_left as f64, top_right as f64, x_diff);
+            let bottom = lerp(bottom_left as f64, bottom_right as f64, x_diff);
+
+            lerp(top, bottom, y_diff).clamp(0., 255.) as u8
+        }).collect();
+
+    // keep the alpha channel as 255;
+    rgba.push(255);
+
+    // it's safe because I am not a fucking idiot
+    rgba
+        .try_into()
+        .unwrap()
+}
 
 pub fn scale_bilinear(image: &CanvasImage, new_width: u32, new_height: u32) -> CanvasImage {
-    let pos = iproduct!(0..new_height, 0..new_width);
     let width = image.width() as f64;
     let height = image.height() as f64;
 
     // transform the positions into a stream of rbga values we can directly copy into our buffer
     // we don't care if the x y actually have a direct mapping, this implementation of bilinear
     // interpolation will handle that for us
-    let rgba = pos.flat_map(|(y, x)| {
-        let x_ratio = x as f64 / (new_width as f64 - 1.);
-        let y_ratio = y as f64 / (new_height as f64 - 1.);
+    let rgba = iproduct!(0..new_height, 0..new_width)
+        .flat_map(|(y, x)| {
+            let x_ratio = x as f64 / (new_width as f64 - 1.);
+            let y_ratio = y as f64 / (new_height as f64 - 1.);
 
 
-        let src_x = (x_ratio * width).min(width - 1.);
-        let src_y = (y_ratio * height).min(height - 1.);
+            let src_x = (x_ratio * width).min(width - 1.);
+            let src_y = (y_ratio * height).min(height - 1.);
 
 
-        let x_diff = src_x - src_x.floor();
-        let y_diff = src_y - src_y.floor();
+            let x_diff = src_x - src_x.floor();
+            let y_diff = src_y - src_y.floor();
 
-        let top_left = image.rgba(src_x.floor() as u32, src_y.floor() as u32).unwrap();
-        let top_right = image.rgba(src_y.ceil() as u32, src_y.floor() as u32).unwrap();
-        let bottom_left = image.rgba(src_x.floor() as u32, src_y.ceil() as u32).unwrap();
-        let bottom_right = image.rgba(src_x.ceil() as u32, src_y.ceil() as u32).unwrap();
+            let top_left = image.rgba(src_x.floor() as u32, src_y.floor() as u32).unwrap();
+            let top_right = image.rgba(src_y.ceil() as u32, src_y.floor() as u32).unwrap();
+            let bottom_left = image.rgba(src_x.floor() as u32, src_y.ceil() as u32).unwrap();
+            let bottom_right = image.rgba(src_x.ceil() as u32, src_y.ceil() as u32).unwrap();
 
-        let top = lerp(
-            top_left.1 as f64,
-            top_right.1 as f64,
-            x_diff,
-        );
-
-        let bottom = lerp(
-            bottom_left.1 as f64,
-            bottom_right.1 as f64,
-            x_diff,
-        );
-
-        let middle = lerp(top, bottom, y_diff);
-
-        let r = middle.clamp(0., 255.) as u8;
-        [0, r, 0, 255]
-    });
+            bi_linear_interpolation(&top_left, &top_right, &bottom_left, &bottom_right, x_diff, y_diff)
+        });
 
     // then we can just copy our transformed stream into a buffer and create a new image from it
     let buffer = Vec::from_iter(rgba);
